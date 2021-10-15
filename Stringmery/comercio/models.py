c3348f87e4b3e735c1c90c4061b118243322a988
andres <af.garciab@uniandes.edu.co>
2021-10-14 23:46:41 -0500

    a

--- Stringmery/comercio/models.py
+++ Stringmery/comercio/models.py
@@ -1,13 +1,13 @@
 from django.db import models
+from producto.models import Producto
 class comercio (models.Model):
 
-
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, default=None)
     nombre= models.CharField(max_length=50)
     dirs = models.CharField(max_length=50)
     login = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     estado = models.CharField(max_length=50)
-
 
 
     def _str_(self):
